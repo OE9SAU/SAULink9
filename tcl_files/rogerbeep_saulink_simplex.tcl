8ea090f8f8011aa7b048f4c8b9a9132df19aaea6
Martin <114362733+OE9SAU@users.noreply.github.com>
2026-05-28 13:36:06 +0200

    Commits

--- tcl_files/rogerbeep_saulink_simplex.tcl
+++ tcl_files/rogerbeep_saulink_simplex.tcl
@@ -1,51 +1,37 @@
 namespace eval Logic {
 
-    variable local_rx 0
+    variable is_network 0
 
-    #
-    # Lokaler RX erkannt
-    #
     proc squelch_open {rx_id is_open} {
 
-        variable local_rx
+        variable is_network
 
-        if {$is_open} {
+        #
+        # Lokale HF beendet
+        #
+        if {!$is_open} {
 
-            set local_rx 1
-
-            # puts "LOCAL RX OPEN"
-            #
-            # Nach 2 Sekunden zurücksetzen
-            #
-            after 2000 {
-                namespace eval Logic {
-                    variable local_rx 0
-                }
-            }
+            set is_network 0
         }
     }
 
     proc send_rgr_sound {} {
 
-        variable local_rx
+        variable is_network
 
-        #
-        # Lokal
-        #
-        if {$local_rx} {
+        if {$is_network} {
+
+            puts "=====>>>>> RGR Netzwerk <<<<<====="
+
+            playTone 500 300 150
+
+        } else {
+
+            puts "=====>>>>> RGR Lokal <<<<<====="
 
             playTone 1000 300 50
             playSilence 80
             playTone 1209 300 50
-			puts "=====>>>>> RGR LOKAL <<<<<====="									  
-
-        #
-        # Netzwerk
-        #
-        } else {
-
-            playTone 500 300 150
-			puts "=====>>>>> RGR NETZWERK <<<<<====="
         }
     }
 }
